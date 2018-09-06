1df63c7dde6d3bec72b12d5e9c2c9f3a5e932f66
John Doe <ajflory9@yahoo.com>
2018-09-06 18:35:32 -0400

    styling and finishing up contact page

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -30,13 +30,24 @@ class Contact extends Component {
                             <div className="contact-list">
                                 <List>
                                     <ListItem>
-                                      <ListItemContent >Bryan Cranston</ListItemContent>
+                                      <ListItemContent style={{fontSize: '25px', fontFamily: 'Didact Gothic'}}>
+                                        <i className="fa fa-phone-square" aria-hidden="true"/>
+                                        954-826-6416
+                                      </ListItemContent>
                                     </ListItem>
+
                                     <ListItem>
-                                      <ListItemContent >Aaron Paul</ListItemContent>
+                                      <ListItemContent style={{fontSize: '25px', fontFamily: 'Didact Gothic'}}>
+                                        <i className="fa fa-envelope" aria-hidden="true"/>
+                                        Ajflory9@yahoo.com
+                                      </ListItemContent>
                                     </ListItem>
+
                                     <ListItem>
-                                      <ListItemContent >Bob Odenkirk</ListItemContent>
+                                      <ListItemContent style={{fontSize: '25px', fontFamily: 'Didact Gothic'}}>
+                                        <i className="fa fa-linkedin" aria-hidden="true"/>
+                                        <a href="https://www.linkedin.com/in/andrewflory/" style={{textDecoration: 'none', color: 'black'}} target="_blank">www.linkedin.com/in/andrewflory/</a>
+                                      </ListItemContent>
                                     </ListItem>
                                 </List>
                             </div>
