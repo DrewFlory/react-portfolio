e86daa9f5f19e0c30f7c27b92169a6a2b32d611c
John Doe <ajflory9@yahoo.com>
2018-09-08 14:18:10 -0400

    working on about me page

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ class App extends Component {
     return (
       <div className="demo-big-content">
     <Layout>
-        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white', fontFamily: 'Didact Gothic', fontSize: '30px'}} to="/">DrewCodez</Link>} scroll>
+        <Header className="header-color" title={<Link style={{textDecoration: 'none', color: 'white', fontFamily: 'Didact Gothic', fontSize: '30px'}} to="/" className="link">DrewCodez</Link>} scroll>
             <Navigation>
-                <Link to="/resume" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}}>Resume</Link>
-                <Link to="/aboutme" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}}>About Me</Link>
-                <Link to="/projects" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}}>Projects</Link>
-                <Link to="/contact" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}}>Contact</Link>
+                <Link to="/resume" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}} className = "link">Resume</Link>
+                <Link to="/aboutme" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}} className = "link">About Me</Link>
+                <Link to="/projects" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}} className = "link">Projects</Link>
+                <Link to="/contact" style= {{fontFamily: 'Didact Gothic', fontSize: '20px'}} className = "link">Contact</Link>
             </Navigation>
         </Header>
             <Drawer title={<Link style={{ fontFamily: 'Didact Gothic', textDecoration: 'none', color: 'black', backgroundSize: '100%', scroll: 'none'}} to="/"><span>My Portfolio</span></Link>}>
--- src/Components/aboutme.js
+++ src/Components/aboutme.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class About extends Component {
     render() {
         return(
-            <div><h1>About Page</h1></div>
+            <div className="about-body">
+                <Grid className="about-grid">
+                    <Cell col={6}>
+                        <img className="about-pic" src="http://www4.pictures.lonny.com/gi/Andrew+Flory+lRmZ0AHIVPcm.jpg"/>
+                    </Cell>
+                    <Cell col={6}>
+                       <p>
+                           My name is Andrew Flory.
+                       </p>
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
